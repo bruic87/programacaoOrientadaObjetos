1817b21c449516a0e299e8d5a257e8420f7a0cc0
bruic87 <bruic@outlook.com>
2023-08-02 09:02:42 -0300

    'refatoracao com composicao de classes'

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ cliente2.nome = "Thaysa";
 
 const contaCorrenteBruno = new ContaCorrente();
 contaCorrenteBruno.agencia = 1001;
+contaCorrenteBruno.cliente = cliente1;
 
-contaCorrenteBruno.depositar(100);
+contaCorrenteBruno.depositar(500);
 
-const valorSacado = contaCorrenteBruno.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 1002;
 
+contaCorrenteBruno.transferir(200, conta2); 
+
+console.log(conta2);
 console.log(contaCorrenteBruno);
