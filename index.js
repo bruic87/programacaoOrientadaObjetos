9557ff22cc150ec98fb587e6d002c3616ff30131
bruic87 <bruic@outlook.com>
2023-08-03 08:40:40 -0300

    'refatoracao com aplicacao de contador estatico'

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente("Bruno", 11122233300);
-
 const cliente2 = new Cliente("Thaysa", 22233344499);
 
-const contaCorrenteBruno = new ContaCorrente(1001, cliente1);
+const contaCorrenteBruno = new ContaCorrente(cliente1, 1001);
 
 contaCorrenteBruno.depositar(500);
 
-const conta2 = new ContaCorrente(1002, cliente2);
+const conta2 = new ContaCorrente(cliente2, 1002);
 
 contaCorrenteBruno.transferir(200, conta2); 
 
-console.log(cliente1);
+console.log(ContaCorrente.numeroDeContas);
