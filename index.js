bc86f4e4e7eca04be8028f9d5d99c64350caa78e
bruic87 <bruic@outlook.com>
2023-08-03 08:28:17 -0300

    'refatoracao com aplicacao de construtores'

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Bruno";
-cliente1.cpf = 11122233300;
+const cliente1 = new Cliente("Bruno", 11122233300);
 
-const cliente2 = new Cliente();
-cliente2.cpf = 22233344499;
-cliente2.nome = "Thaysa";
+const cliente2 = new Cliente("Thaysa", 22233344499);
 
-const contaCorrenteBruno = new ContaCorrente();
-contaCorrenteBruno.agencia = 1001;
-contaCorrenteBruno.cliente = cliente1;
+const contaCorrenteBruno = new ContaCorrente(1001, cliente1);
 
 contaCorrenteBruno.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
+const conta2 = new ContaCorrente(1002, cliente2);
 
 contaCorrenteBruno.transferir(200, conta2); 
 
-console.log(conta2);
-console.log(contaCorrenteBruno);
+console.log(cliente1);
